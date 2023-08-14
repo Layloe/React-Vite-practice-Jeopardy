cffee4cd3dabed7a145f5ac982c51e15589d3830
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2023-08-14 06:13:28 -0700

    first commit

---
+++ src/Category.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Clue from './Clue'
+import './category.css'
+
+function Category({category}) {
+const values = [200, 400, 600, 800, 1000]
+
+const toTitleCase = (str) => {
+    return str
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+  };
+
+  const title = toTitleCase(category.title)
+  
+    return(
+        //header and five clues
+        <div className="jeopardy-category">
+            <h2>{category.title}</h2>
+            {values.map((value,index) => {
+                const clue = category.clues.find((clue) => clue.value === value)
+                return <Clue key={index} value={value} clue={clue}/>
+            })}
+        </div>
+    )
+}
+
+export default Category
